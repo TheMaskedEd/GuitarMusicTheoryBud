0519bd59c04b0750230f62136328390d0bc55050
themaskeded <edwardcgoodall@gmail.com>
2023-04-26 19:16:23 -0500

    1.0.1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import Plot
 #dictionary of scales and their intervals
 scales = {
         "major": [0, 2, 4, 5, 7, 9, 11],
-        "minor": [0, 2, 3, 5, 7, 10, 11],
+        "minor": [0, 2, 3, 5, 7, 8, 10, 12],
         "dorian": [0, 2, 3, 5, 7, 9, 10, 12],
         "phrygian": [0, 1, 3, 5, 7, 8, 10, 12],
         "minor pentatonic": [0, 3, 5, 7, 10],

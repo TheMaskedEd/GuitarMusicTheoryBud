29a8ba929dbff8f1f229a27e5efea9f2717f8973
themaskeded <edwardcgoodall@gmail.com>
2023-04-26 21:39:49 -0500

    1.0.1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ scales = {
         "phrygian": [0, 1, 3, 5, 7, 8, 10, 12],
         "minor pentatonic": [0, 3, 5, 7, 10],
         "major pentatonic": [0, 2, 4, 7, 9],
-        "harmonic minor": [0, 2, 3, 5, 7, 8, 10, 12],
+        "harmonic minor": [0, 2, 3, 5, 7, 10, 11],
         "mixolydian": [0, 2, 4, 5, 7, 9, 10],
         "minor blues": [0, 3, 5, 6, 7, 10],
         "locrian": [0, 1, 3, 5, 6, 8, 10, 12],
